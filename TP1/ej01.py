3d8f01eb2c2328c8392abb5183ceca08c00aa628
jjuanisima <jl9773788@gmail.com>
2024-09-07 17:45:32 -0300

    exercise 2 finished

---
+++ TP1/ej01.py
@@ -0,0 +1,70 @@
+'''
+ 1. Desarrollar una función que reciba tres números enteros positivos y devuelva el 
+mayor de los tres, sólo si éste es único (es decir el mayor estricto). Devolver -1 en 
+caso de no haber ninguno. No utilizar operadores lógicos (and, or, not). Desarrollar 
+también un programa para ingresar los tres valores, invocar a la función y mostrar 
+el máximo hallado, o un mensaje informativo si éste no existe.
+'''
+
+
+def mayor_estricto(a: int, b: int, c: int) -> int:
+    '''
+    Busca al mayor estricto entre los tres números.
+
+    Pre:
+        a (int): número A
+        b (int): número B
+        c (int): número C
+    Post:
+        int a, b ó c: el que resulte ser el mayor estricto entre los tres
+        int -1: los tres números son iguales, o sea que no hay un mayor estricto
+    '''
+    mayor = -1
+
+    if a > b:
+        if a > c:
+            if a != b:
+                if a != c:
+                    mayor = a
+
+    if b > a:
+        if b > c:
+            if b != a:
+                if b != c:
+                    mayor = b
+
+    if c > a:
+        if c > b:
+            if c != a:
+                if c != b:
+                    mayor = c
+
+    return mayor
+
+
+def main() -> None:
+    '''
+    Función principal, donde el usuario ingresa los tres números solicitados
+
+    Pre:
+        la función no recibe parámetros
+    Post:
+        la función no devuelve ningún valor
+    '''
+    try:
+        a = int(input("Número A: "))
+        b = int(input("Número B: "))
+        c = int(input("Número C: "))
+
+        if a < 1 or b < 1 or c < 1:
+            print("ERROR. Intenta con números positivos.")
+
+        if mayor_estricto(a, b, c) == -1:
+            print("Los tres números ingresados son iguales :|")
+        else:
+            print(f"El mayor estricto es el número {mayor_estricto(a, b, c)}")
+    except ValueError:
+        print("ERROR. Intenta ingresando números enteros.")
+    return None
+
+main()
---
+++ TP1/ej02.py
@@ -0,0 +1,73 @@
+'''
+Desarrollar una función que reciba tres números enteros positivos correspondientes 
+al día, mes, año de una fecha y verifique si corresponden a una fecha válida. Debe 
+tenerse en cuenta la cantidad de días de cada mes, incluyendo los años bisiestos. 
+Devolver True o False según la fecha sea correcta o no. Realizar también un 
+programa para verificar el comportamiento de la función.
+'''
+
+
+def fecha_valida(dia: int, mes: int, anio: int) -> bool:
+    '''
+    Verifica si la fecha ingresada es válida
+
+    Pre:
+        dia (int): día del mes
+        mes (int): mes del año
+        anio (int): el año
+    Post:
+        bool True: la fecha es válida
+        bool False: la fecha es inválida
+    '''
+
+    if mes < 1 or mes > 12:
+        return False
+    if anio < 1:
+        return False
+    if mes in (4, 6, 9, 11):
+        return dia >= 1 and dia <= 30
+    if mes in (1, 3, 5, 7, 8, 10, 12):
+        return dia >= 1 and dia <= 31
+    if mes == 2:
+        if anio_bisiesto(anio):
+            return dia <= 29
+        return dia <= 28
+
+def anio_bisiesto(anio: int) -> bool:
+    '''
+    Verifica si el año es bisiesto
+
+    Pre:
+        anio (int): el año a verificar
+    Post:
+        bool True: si es divisible por 400 o también es divisible por 4 pero no por 100, entonces el año es bisiesto
+        bool False: si no se cumple la condición, el año no es bisiesto
+    '''
+
+    return (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0)
+
+def main() -> None:
+    '''
+    Función principal, donde el usuario ingresa la fecha a validar
+
+    Pre:
+        la función no recibe parámetros
+    Post:
+        la función no devuelve ningún valor
+    '''
+
+    while True:
+        try:
+            dia = int(input("Día: "))
+            mes = int(input("Mes: "))
+            anio = int(input("Año: "))
+            
+            if fecha_valida(dia, mes, anio):
+                print(f"La fecha {dia}/{mes}/{anio} es válida :)\n")
+            else:
+                print(f"La fecha {dia}/{mes}/{anio} es inválida :(\n")
+        except ValueError:
+            print("ERROR. Intenta de nuevo ingresando números enteros.\n")
+    return None
+
+main()

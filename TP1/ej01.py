b1a91c2df4ad83e468e7b12244c03ac2f1385d12
jjuanisima <jl9773788@gmail.com>
2024-09-09 23:53:45 -0300

    exercise 1 finished

--- TP1/ej01.py
+++ TP1/ej01.py
@@ -1,5 +1,5 @@
 '''
- 1. Desarrollar una función que reciba tres números enteros positivos y devuelva el 
+Desarrollar una función que reciba tres números enteros positivos y devuelva el 
 mayor de los tres, sólo si éste es único (es decir el mayor estricto). Devolver -1 en 
 caso de no haber ninguno. No utilizar operadores lógicos (and, or, not). Desarrollar 
 también un programa para ingresar los tres valores, invocar a la función y mostrar 

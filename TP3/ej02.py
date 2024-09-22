6acc203480f42e8432ea17f7efb7c0d24f2ad1f6
jjuanisima <jl9773788@gmail.com>
2024-09-22 18:13:17 -0300

    exercise 2 finished (no)

--- TP3/ej02.py
+++ TP3/ej02.py
@@ -7,12 +7,12 @@ cerse como N x N, donde N se ingresa a través del teclado.
 
 def matriz_a(n: int) -> list:
     '''
-    Crea una matriz de N * N con números impares en la diagonal y ceros en las demás posiciones
+    Crea una matriz de N * N con números impares crecientes en la diagonal
 
     Pre:
         n (int): valor de n, que define el tamaño de la matriz
     Post:
-        matriz (list): la matriz con ceros y números impares en diagonal
+        matriz (list): la matriz con los elementos modificados
     '''
     
     matriz = [[0] * n for i in range(n)]
@@ -22,6 +22,91 @@ def matriz_a(n: int) -> list:
             if i == j:
                 matriz[i][j] = num_impar
                 num_impar += 2
+    return matriz
+
+def matriz_b(n: int) -> list:
+    '''
+    Crea una matriz de N * N con potencias decrecientes de 3 en la diagonal inversa
+
+    Pre:
+        n (int): valor de n, que define el tamaño de la matriz
+    Post:
+        matriz (list): la matriz con los elementos modificados
+    '''
+
+    matriz = [[0] * n for i in range(n)]
+    num = (3 ** (n - 1))
+    for i in range(len(matriz)):
+        matriz[i][n-1-i] = num
+        num //= 3
+    return matriz
+
+def matriz_c(n: int) -> list:
+    '''
+    Crea una matriz de N * N con números decrecientes en la diagonal principal y debajo de ella
+
+    Pre:
+        n (int): valor de n, que define el tamaño de la matriz
+    Post:
+        matriz (list): la matriz con los elementos modificados
+    '''
+
+    matriz = [[0] * n for i in range(n)]
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            if i == j or i > j:
+                matriz[i][j] = n - i
+    return matriz
+
+def matriz_d(n: int) -> list:
+    '''
+    Crea una matriz de N * N con potencias decrecientes de 2 en cada fila
+
+    Pre:
+        n (int): valor de n, que define el tamaño de la matriz
+    Post:
+        matriz (list): la matriz con los elementos modificados
+    '''
+
+    matriz = [[(2 ** (n - 1) // (2 ** i))] * n for i in range(n)]
+    return matriz
+
+def matriz_e(n: int) -> list:
+    '''
+    Crea una matriz de N * N con ceros intercalados con números crecientes
+
+    Pre:
+        n (int): valor de n, que define el tamaño de la matriz
+    Post:
+        matriz (list): la matriz con los elementos modificados
+    '''
+
+    matriz = [[0] * n for i in range(n)]
+    num = 1
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            if (i + j) % 2 != 0:
+                matriz[i][j] = num
+                num += 1
+    return matriz
+
+def matriz_f(n: int) -> list:
+    '''
+    Crea una matriz de N * N con números consecutivos en la diagonal inversa y abajo de ella
+
+    Pre:
+        n (int): valor de n, que define el tamaño de la matriz
+    Post:
+        matriz (list): la matriz con los elementos modificados
+    '''
+    
+    matriz = [[0] * n for i in range(n)]
+    num = 1
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            if i == j or i > j:
+                matriz[i][n-1-j] = num
+                num += 1
     return matriz
 
 def printear_matriz(matriz: list) -> None:
@@ -41,5 +126,20 @@ def main() -> None:
 
     print("\nMatriz a:")
     printear_matriz(matriz_a(n))
+
+    print("\nMatriz b:")
+    printear_matriz(matriz_b(n))
+
+    print("\nMatriz c:")
+    printear_matriz(matriz_c(n))
+
+    print("\nMatriz d:")
+    printear_matriz(matriz_d(n))
+
+    print("\nMatriz e:")
+    printear_matriz(matriz_e(n))
+
+    print("\nMatriz f:")
+    printear_matriz(matriz_f(n))
 
 main()

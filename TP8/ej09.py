a42cc52ede093ece90b1e38fb79e8bf4754c003e
jjuanisima <jl9773788@gmail.com>
2024-11-06 11:36:49 -0300

    modified exercise 9

--- TP8/ej09.py
+++ TP8/ej09.py
@@ -15,22 +15,7 @@ def tabla_multiplicar(n: int) -> dict:
         devuelve un diccionario donde las claves son los números del 1 al 12 y los valores son los resultados de multiplicar n por esas claves
     '''
 
-    diccionario = {i: n * i for i in range(1, 13)}
-    return diccionario
-
-def mostrar_tabla(tabla: dict, n: int) -> None:
-    '''
-    Printea la tabla de multiplicar en un formato más legible
-
-    Pre:
-        tabla (dict): el diccionario que contiene la tabla de multiplicar
-        n (int): el número del cual se muestra la tabla
-    Post:
-        la función no devuelve ningún valor, sólo imprime la tabla
-    '''
-
-    for multiplo, resultado in tabla.items():
-        print(f"{n} x {multiplo} = {resultado}")
+    return {i: n * i for i in range(1, 13)}
 
 def main() -> None:
     '''
@@ -42,7 +27,8 @@ def main() -> None:
     try:
         n = int(input("Número entero: "))
         tabla = tabla_multiplicar(n)
-        print(mostrar_tabla(tabla, n))
+        for multiplo, resultado in tabla.items():
+            print(f"{n} x {multiplo} = {resultado}")
     except ValueError:
         print("ERROR. Revisa de ingresar un número entero :|")
 

03486208913da8d61337459956309b65efccb816
jjuanisima <jl9773788@gmail.com>
2024-11-20 18:01:22 -0300

    modified exercise 1

--- TP1/ej01.py
+++ TP1/ej01.py
@@ -12,52 +12,30 @@ def mayor_estricto(a: int, b: int, c: int) -> int:
     Busca al mayor estricto entre los tres números.
 
     Pre:
-        a (int): número A
-        b (int): número B
-        c (int): número C
+        a, b, c (int): números enteros positivos
     Post:
-        int a, b ó c: el que resulte ser el mayor estricto entre los tres
-        int -1: los tres números son iguales, o sea que no hay un mayor estricto
+        devuelve el mayor estricto, en caso contrario -1
     '''
 
-    mayor = -1
+    numeros = [a, b, c]
+    mayor = max(numeros)
+    if numeros.count(mayor) == 1:
+        return mayor
+    return -1
 
-    if a > b:
-        if a > c:
-            if a != b:
-                if a != c:
-                    mayor = a
-    if b > a:
-        if b > c:
-            if b != a:
-                if b != c:
-                    mayor = b
-    if c > a:
-        if c > b:
-            if c != a:
-                if c != b:
-                    mayor = c
-    return mayor
-
-def main() -> None:
-    '''
-    Función principal, donde el usuario ingresa los tres números solicitados
-    '''
-
+if __name__ == "__main__":
     try:
         a = int(input("Número A: "))
         b = int(input("Número B: "))
         c = int(input("Número C: "))
 
         if a < 1 or b < 1 or c < 1:
-            print("ERROR. Intenta con números positivos.")
-
-        if mayor_estricto(a, b, c) == -1:
-            print("Los tres números ingresados son iguales :|")
+            print("ERROR. Intenta con números positivos :|")
         else:
-            print(f"El mayor estricto es el número {mayor_estricto(a, b, c)}")
+            resultado = mayor_estricto(a, b, c)
+            if resultado == -1:
+                print("Los tres números ingresados son iguales :|")
+            else:
+                print(f"El mayor estricto es el número {resultado} :)")
     except ValueError:
         print("ERROR. Intenta ingresando números enteros.")
-    return None
-
-main()

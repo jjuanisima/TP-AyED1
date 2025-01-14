baaa5b88553a98b39b3fe5275dc04ff7f70fe25d
jjuanisima <jl9773788@gmail.com>
2025-01-14 17:39:09 -0300

    modified exercise 2 (v2)

--- TP1/ej02.py
+++ TP1/ej02.py
@@ -16,30 +16,30 @@ def fecha_valida(dia: int, mes: int, anio: int) -> bool:
     Post:
         devuelve True si la fecha es válida, en caso contrario False
     '''
-
     if mes < 1 or mes > 12 or dia < 1 or anio < 1:
         return False
     
     if mes == 2:
-        if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0):
-            return dia <= 29
-        else:
-            return dia <= 28
+        return dia <= 29 if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0) else dia <= 28
         
-    if mes in (4, 6, 9, 11):
-        return dia <= 30
+    return dia <= 30 if mes in (4, 6, 9, 11) else dia <= 31
     
-    return dia <= 31
-
-if __name__ == "__main__":
+def main() -> None:
+    '''
+    Función principal que maneja el flujo del programa
+    No recibe parámetros ni devuelve ningún valor
+    '''
     try:
         dia = int(input("Día: "))
         mes = int(input("Mes: "))
         anio = int(input("Año: "))
-        
-        if fecha_valida(dia, mes, anio):
-            print(f"La fecha {dia}/{mes}/{anio} es válida :)\n")
-        else:
-            print(f"La fecha {dia}/{mes}/{anio} es inválida :(\n")
     except ValueError:
         print("ERROR. Ingresa números enteros válidos :|\n")
+        
+    if fecha_valida(dia, mes, anio):
+        print(f"La fecha {dia}/{mes}/{anio} es válida :)\n")
+    else:
+        print(f"La fecha {dia}/{mes}/{anio} es inválida :(\n")
+
+if __name__ == "__main__":
+    main()
